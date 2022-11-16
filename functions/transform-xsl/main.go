4b14f0305dc3709d6cf4005bdc61fb91388dba78
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-11-16 10:34:39 -0500

    excel rows work

--- functions/transform-xsl/main.go
+++ functions/transform-xsl/main.go
@@ -47,12 +47,12 @@ type Evento struct {
 
 func Handler(ctx context.Context, ev Evento) (string, error) {
 
-	var TABLE_NAME = os.Getenv("TABLA_NAME")
+	// var TABLE_NAME = os.Getenv("TABLA_NAME")
 	var BUCKET_NAME = os.Getenv("BUCKET_NAME")
 	var OBJECT_NAME = ev.Object.Key
-	var cliente Cliente
-	var credito Credito
-	var certificado Certificado
+	// var cliente Cliente
+	// var credito Credito
+	// var certificado Certificado
 
 	//Iniciar sesion en aws
 	sess, err := session.NewSession(&aws.Config{
@@ -63,8 +63,8 @@ func Handler(ctx context.Context, ev Evento) (string, error) {
 		return "", err
 	}
 
-	svcDynamo := dynamodb.New(sess) // Dynamodb
-	svcS3 := s3.New(sess)           // s3
+	// svcDynamo := dynamodb.New(sess) // Dynamodb
+	svcS3 := s3.New(sess) // s3
 
 	file, err := svcS3.GetObject(
 		&s3.GetObjectInput{
@@ -78,81 +78,82 @@ func Handler(ctx context.Context, ev Evento) (string, error) {
 
 	result, _ := OpenFile(*file)
 
-	cell1 := result.GetCellValue("Sheet1", "A2")
-	cell2 := result.GetCellValue("Sheet1", "B2")
-	cell3 := result.GetCellValue("Sheet1", "C2")
-	cell4 := result.GetCellValue("Sheet1", "D2")
-	cell5 := result.GetCellValue("Sheet1", "E2")
-	cell6 := result.GetCellValue("Sheet1", "F2")
+	// cell1 := result.GetCellValue("Sheet1", "A2")
+	// cell2 := result.GetCellValue("Sheet1", "B2")
+	// cell3 := result.GetCellValue("Sheet1", "C2")
+	// cell4 := result.GetCellValue("Sheet1", "D2")
+	// cell5 := result.GetCellValue("Sheet1", "E2")
+	// cell6 := result.GetCellValue("Sheet1", "F2")
 
-	cliente = Cliente{
-		PK:     "Cliente",
-		Sk:     cell3,
-		TIdDoc: cell2,
-		NIdDoc: cell3,
-		Name:   cell4,
-	}
-
-	credito = Credito{
-		PK:       "Credito",
-		Sk:       cell1,
-		Currency: cell5,
-	}
-
-	certificado = Certificado{
-		PK:    "Certificado",
-		Sk:    cell1,
-		Prime: cell6,
-	}
-
-	data1, _ := MarshalMap(cliente)
-	data2, _ := MarshalMap(credito)
-	data3, _ := MarshalMap(certificado)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return "marshal error", err
-	}
-	params1 := &dynamodb.PutItemInput{
-		Item:      data1,
-		TableName: aws.String(TABLE_NAME),
-	}
-	_, err2 := svcDynamo.PutItem(params1)
-
-	if err2 != nil {
-		fmt.Println(err2.Error())
-		return "dynamo error", err2
-	}
-
-	params2 := &dynamodb.PutItemInput{
-		Item:      data2,
-		TableName: aws.String(TABLE_NAME),
-	}
-	_, err3 := svcDynamo.PutItem(params2)
-
-	if err3 != nil {
-		fmt.Println(err2.Error())
-		return "dynamo error", err2
-	}
-
-	params3 := &dynamodb.PutItemInput{
-		Item:      data3,
-		TableName: aws.String(TABLE_NAME),
-	}
-	_, err4 := svcDynamo.PutItem(params3)
-
-	if err4 != nil {
-		fmt.Println(err2.Error())
-		return "dynamo error", err2
-	}
-
-	// rows := result.GetRows("Sheet1")
-
-	// for _, row := range rows {
-	// 	for _, colCell := range row {
-	// 		fmt.Print(colCell, "\t")
-	// 	}
+	// cliente = Cliente{
+	// 	PK:     "Cliente",
+	// 	Sk:     cell3,
+	// 	TIdDoc: cell2,
+	// 	NIdDoc: cell3,
+	// 	Name:   cell4,
 	// }
+
+	// credito = Credito{
+	// 	PK:       "Credito",
+	// 	Sk:       cell1,
+	// 	Currency: cell5,
+	// }
+
+	// certificado = Certificado{
+	// 	PK:    "Certificado",
+	// 	Sk:    cell1,
+	// 	Prime: cell6,
+	// }
+
+	// data1, _ := MarshalMap(cliente)
+	// data2, _ := MarshalMap(credito)
+	// data3, _ := MarshalMap(certificado)
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return "marshal error", err
+	// }
+	// params1 := &dynamodb.PutItemInput{
+	// 	Item:      data1,
+	// 	TableName: aws.String(TABLE_NAME),
+	// }
+	// _, err2 := svcDynamo.PutItem(params1)
+
+	// if err2 != nil {
+	// 	fmt.Println(err2.Error())
+	// 	return "dynamo error", err2
+	// }
+
+	// params2 := &dynamodb.PutItemInput{
+	// 	Item:      data2,
+	// 	TableName: aws.String(TABLE_NAME),
+	// }
+	// _, err3 := svcDynamo.PutItem(params2)
+
+	// if err3 != nil {
+	// 	fmt.Println(err2.Error())
+	// 	return "dynamo error", err2
+	// }
+
+	// params3 := &dynamodb.PutItemInput{
+	// 	Item:      data3,
+	// 	TableName: aws.String(TABLE_NAME),
+	// }
+	// _, err4 := svcDynamo.PutItem(params3)
+
+	// if err4 != nil {
+	// 	fmt.Println(err2.Error())
+	// 	return "dynamo error", err2
+	// }
+
+	rows := result.GetRows("Sheet1")
+	fmt.Println(len(rows))
+
+	for _, row := range rows {
+		for _, colCell := range row {
+			fmt.Print(colCell, "\t")
+		}
+	}
 	return "success", nil
 }
 
